ae131ff265f2f92a7cb63b9b7442c2b4e5ff8036
JoaoBarroso25 <a95195@alunos.uminho.pt>
2023-11-10 22:23:47 +0000

    JobFuction class

---
+++ src/main/java/JobFunction/JobFunctionException.java
@@ -0,0 +1,12 @@
+package JobFunction;
+public class JobFunctionException extends Exception {
+    private static final int CODE = 138;
+
+    public JobFunctionException(String var1, Throwable var2) {
+        super(var1, var2);
+    }
+
+    public int getCode() {
+        return 138;
+    }
+}
